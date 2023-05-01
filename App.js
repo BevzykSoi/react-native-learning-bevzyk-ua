c84c5d80d44685a35cfdb45237a98fe8f9bdc110
Bevzyk SOI <bevzyk.soi@gmail.com>
2023-05-01 17:25:44 +0200

    Completed 02-new-features

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import moment from 'moment/moment';
 
 export default function App() {
+  let newVariable = 0;
+  const myCredentials = {
+    name: "Bevzyk UA",
+    age: 13,
+  };
+
+  const variablePlusOne = (variable) => {
+    return variable + 1;
+  };
+  const greeting = ({name, age}) => {
+    return `Hello, ${name}. You are ${age} years old!`
+  };
+
+  console.log(greeting(myCredentials));
+  console.log(variablePlusOne(newVariable));
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Today is {moment().format("MMMM Do YYYY")}
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -18,7 +37,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    fontSize: 22,
-    color: 'blue'
+    fontFamily: 'Roboto',
+    fontSize: 38,
   }
 });
